77c72ad6390004d5d85b8f97be382062a1412f95
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-11-06 20:09:25 +0300

    feat: add music to main menu

--- quiz/src/modules/_homepage.js
+++ quiz/src/modules/_homepage.js
@@ -1,15 +1,26 @@
 import { goToQuizPage, resetState } from './_quizpage';
 import 'animate.css';
+import mainMenuThemeSrc from '../assets/audio/main-menu.mp3';
+
+const mainMenuTheme = new Audio();
+mainMenuTheme.src = mainMenuThemeSrc;
+mainMenuTheme.loop = true;
+mainMenuTheme.volume = 0.5;
 
 function createStartButton() {
   const startButton = document.createElement('button');
   startButton.classList.add('main__start-button', 'animate__animated', 'animate__pulse', 'animate__infinite', 'animate__slow');
   startButton.innerHTML = 'START';
-  startButton.onclick = goToQuizPage;
+  startButton.addEventListener('click', () => {
+    goToQuizPage();
+    mainMenuTheme.pause();
+  });
   return startButton;
 }
 
 function goToHomePage() {
+  mainMenuTheme.currentTime = 0;
+  mainMenuTheme.play();
   resetState(true);
   document.querySelector('.main').innerHTML = '';
   document.querySelector('.main').append(createStartButton());
@@ -17,6 +28,7 @@ function goToHomePage() {
 }
 
 function createMain() {
+  mainMenuTheme.play();
   const main = document.createElement('main');
   main.className = 'main';
   const startButton = createStartButton();
